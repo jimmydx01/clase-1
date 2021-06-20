4fdfa46fbc47de222db34eb4839d14270002444f
jimmydx01 <84819462+jimmydx01@users.noreply.github.com>
2021-06-19 20:52:34 -0500

    Add files via upload

---
+++ clase3.py
@@ -0,0 +1,88 @@
+class For:
+    def _init_(self):
+        pass
+    #ciclo repetitivo de incrementos o decrementos se ejecuta por verdadero
+    def usoFor(self):
+        datos=["jimmy",50,True]
+        cifras=(2,9,6,4,1)
+        docente={"nombre":"jimmy","age":50,"fac",:"faci"}
+        listadenotas=[(30,40)(20,40)(50,40)]
+        listadealumnos=[{"nombre":"luis","final":70},{"nombre""Ana","final":60}{"nombre""Marco","final":50}]
+        # for i range (5 ): #rango(0,1,2,3,4)
+        #print("i={}".format(i))
+        #for i  in range (2,10):# rango(2,3,4,5,6,7,8,9)
+        #    print("i={}".format(i))
+        #for i in  range(4,10,2): #rango(4,6,8)
+         #   print("i={}".format(i),end=" ")
+        
+        #longuitud=len(datos)
+        #print(datos[0])
+        #print(datos[1])
+        #print(datos[2])
+        #j=0
+        #while j < longuitud:
+            #print("while",datos[j])
+            #j=j+1
+
+
+        #for i in range(longuitud-1,-1,-1):
+            #print(datos[i])
+
+        #for i,dato in enumerate(numeros):
+            #print("for",i,dato)
+        #for dato in numeros:
+            #print(dato) 
+
+        #for dato in ["H","O","L","A","QUE","TE","CUENTAS"]:
+            #print(dato)
+
+
+        print("\nDICCIONARIO de NOtas")
+        # for clave,valor in docente.items():
+        #     print(clave,":"valor,end="  ")
+
+        # for alumnos in listaAlumnos
+        #     for clave,valor in alumnos.items():
+        #         print(clave,":",valor,end="   ")
+                
+        # listNotas=[(30,40),[20,40,20],(50,40,20,10,5)]
+        # acum=0
+        # long=0
+        # for notas in listNotas:
+        #     parcial=0
+        #     print(notas,end=)
+        #     for nota in notas:
+        #         print(nota)
+        #         long=long+1
+        #         acum=acum+nota
+        #         parcial += nota
+        #     promparcial= parcial/len(notas)
+        #     print("notas parcial={} promedio parcial={}".format(nota,parcial))
+        # prom=acum/long    
+        # print("total notas={}- #notas={}: promedio={} " ,format(acum,long,prom))
+        # listaAlumnos=[{"nombre":"luis","final":70},{"nombre""Ana","final":60}{"nombre""Marco","final":50}]
+        # acum=0
+        # cont=0
+        # for alumnos in listaAlumnos
+        #     print(alumnos)
+        #     for clave,valor in alumnos.items():
+        #         print(clave,":",valor,end="  ")
+        #         if clave=="final":acum=acum+valor
+        #     cont+=1    
+        # print(acum/cont)    
+
+        # frase = "hola como te  encuentras "
+        # vocales=[]
+        # for car in frase:
+        #     if car in("a" "e" "i" "o" "u")
+        #         vocales.append(car)
+        
+        print([car for car in "holo como te encuentras " if car in (" ")])
+
+
+                
+
+
+bucle1= For()
+bucle1.usoFor()         
+
